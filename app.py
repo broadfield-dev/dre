ca63c9633b0c001b7f009e96709c4e2a1de1694e
broadfield-dev <broadfield.apps@gmail.com>
2025-11-15 05:07:05 -0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+
+def mod_dep(file_path=".",orig_str="",mod_str=""):
+  file_path =file_path
+  
+  try:
+      file_content = index_path.read_text()
+      original_block = orig_str
+      replacement_block = mod_str
+      if original_block in file_content:
+          modified_content = file_content.replace(original_block, replacement_block)
+          
+          index_path.write_text(modified_content)
+          print("Script modified successfully.")
+      else:
+          print("Warning: Error")
+  
+  except Exception as e:
+      print(f"An error occurred while modifying the script: {e}")
+      sys.exit(1)
+      
