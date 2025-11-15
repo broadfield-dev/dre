78f1ae59a70360219ae7f4c21ed50e1b5dfc0bb7
broadfield-dev <broadfield.apps@gmail.com>
2025-11-15 05:35:24 -0800

    Update app.py

--- engine/app.py
+++ engine/app.py
@@ -4,13 +4,13 @@ def mod_dep(file_path=".",orig_str="",mod_str=""):
   file_path =file_path
   
   try:
-      file_content = index_path.read_text()
+      file_content = file_path.read_text()
       original_block = orig_str
       replacement_block = mod_str
       if original_block in file_content:
           modified_content = file_content.replace(original_block, replacement_block)
           
-          index_path.write_text(modified_content)
+          file_path.write_text(modified_content)
           print("Script modified successfully.")
       else:
           print("Warning: Error")
